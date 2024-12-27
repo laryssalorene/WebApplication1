b420174d89c9b6c85a9868f0d0cb4dc1653e8955
Laryssa <laryssalo@edu.unifor.br>
2024-12-27 12:25:28 -0300

    modificações para integração e consumo da API Via Cep

---
+++ WebApplication1/Integracao/ViaCepIntegracao.cs
@@ -0,0 +1,25 @@
+﻿using SistemaDeTarefas.Integracao.Interfaces;
+using SistemaDeTarefas.Integracao.Refit;
+using SistemaDeTarefas.Integracao.Response;
+
+namespace SistemaDeTarefas.Integracao
+{
+    public class ViaCepIntegracao :IViaCepIntegracao
+    {
+        //implementação da chamada do Refit para o serviço de integração
+        private readonly IViaCepIntegracaoRefit _viaCepIntegracaoRefit; 
+        public ViaCepIntegracao(IViaCepIntegracaoRefit viaCepIntegracaoRefit)
+        {
+            _viaCepIntegracaoRefit = viaCepIntegracaoRefit;
+        }
+        public async Task<ViaCepResponse> ObterDadosViaCep(string cep)
+        {
+           var responseData = await _viaCepIntegracaoRefit.ObterDadosViaCep(cep);
+            if (responseData != null && responseData.IsSuccessStatusCode)
+            {
+                return responseData.Content;
+            }
+            return null;
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,10 @@
 
 using Microsoft.EntityFrameworkCore;
+using Refit;
 using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Integracao;
+using SistemaDeTarefas.Integracao.Interfaces;
+using SistemaDeTarefas.Integracao.Refit;
 using SistemaDeTarefas.Repositorios;
 using SistemaDeTarefas.Repositorios.Interfaces;
 
@@ -32,7 +36,13 @@ namespace SistemaDeTarefas
             //instanciada a classe UsuarioRepositorio
             builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
             builder.Services.AddScoped<ITarefaRepositorio, TarefaRepositorio>();
+            builder.Services.AddScoped<IViaCepIntegracao, ViaCepIntegracao>();
 
+
+            builder.Services.AddRefitClient<IViaCepIntegracaoRefit>().ConfigureHttpClient(c =>
+            {
+                c.BaseAddress = new Uri("https://viacep.com.br");
+            });
             //////////////
             var app = builder.Build();
 
